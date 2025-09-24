a8e49cc809a06848bffc4c3e1345adfcb4ee9029
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-09-24 14:57:00 +0530

    Moved the builder to mainfile

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    ac_installer_gui_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
